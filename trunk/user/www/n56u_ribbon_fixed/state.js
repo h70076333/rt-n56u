2b829fb3eac24517644cac74117fcdf5fca47c15
h70076333 <h70076222@88.com>
2025-03-12 02:00:24 +0800

    Update state.js

--- trunk/user/www/n56u_ribbon_fixed/state.js
+++ trunk/user/www/n56u_ribbon_fixed/state.js
@@ -522,7 +522,7 @@ if (found_app_aliddns()){
 	ddnsto_array = new Array("","Advanced_ddnsto.asp");
 	tablink[16] = (ddnsto_array);
 }else if (found_app_zerotier()){
-	zerotier_array = new Array("","Advanced_zerotier.asp");
+	zerotier_array = new Array("","Advanced_vpnkey.asp");
 	tablink[16] = (zerotier_array);
 }else if (found_app_wireguard()){
 	wireguard_array = new Array("","Advanced_wireguard.asp");
